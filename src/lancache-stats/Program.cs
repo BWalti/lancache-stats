34836658300ecf44ef1e70a7845f0425ed94a11d
Beat Walti <Beat_Walti@epam.com>
2022-10-19 15:21:36 +0200

    performance: reduced the amount of console re-drawings

--- src/lancache-stats/Program.cs
+++ src/lancache-stats/Program.cs
@@ -6,8 +6,6 @@ using lancache_stats.Parser;
 using lancache_stats.Stats;
 using Spectre.Console;
 
-await Task.Delay(1000);
-
 var path = !args.Any() ? Directory.GetCurrentDirectory() : args[0];
 var reader = new AccessLogReader(path);
 var enumerator = reader.ReadContent().GetAsyncEnumerator();
@@ -15,18 +13,19 @@ var enumerator = reader.ReadContent().GetAsyncEnumerator();
 var parser = new AccessLogParser();
 var statsBuilder = new StatsBuilder();
 
-var outerTable = new Table().Centered();
+var outerTable = new Table().Expand();
 outerTable.AddColumn("");
 
-var table = new Table().Centered();
-table.Title("[bold]Total[/]");
+var table = new Table().Expand();
+
+table.Title("[[ [bold]Total[/] ]]");
+table.SimpleHeavyBorder();
 table.AddColumn("[bold]System[/]");
 table.AddColumn("Hits");
 table.AddColumn("Misses");
 table.AddColumn("Cache Reads");
 table.AddColumn("Remote Reads");
 outerTable.AddRow(table);
-
 
 var rowCounter = 0;
 var itemCountLookup = new Dictionary<string, int>();
@@ -35,37 +34,42 @@ await AnsiConsole
     .Live(outerTable)
     .StartAsync(async ctx =>
     {
+        var counter = 0;
         while (await enumerator.MoveNextAsync())
         {
             var line = enumerator.Current;
             var result = parser.Parse(line);
             statsBuilder.Apply(result);
+            counter++;
 
-            foreach (var stats in statsBuilder.SystemStats)
+            if (counter % 10000 == 0)
             {
-                var valueStats = stats.Value.Stats;
-
-                var hitCell = $"[lime]{valueStats.Hits.ToMetric()}[/]";
-                var missCell = $"[red]{valueStats.Misses.ToMetric()}[/]";
-                var cachedBytesCell = $"[lime]{ByteSize.FromBytes(valueStats.CachedBytes)}[/]";
-                var remoteBytesCell = $"[red]{ByteSize.FromBytes(valueStats.RemoteBytes)}[/]";
-
-                if (!itemCountLookup.TryGetValue(stats.Key, out var itemIndex))
+                foreach (var stats in statsBuilder.SystemStats)
                 {
-                    itemIndex = rowCounter++;
-                    itemCountLookup.Add(stats.Key, itemIndex);
-                    table.AddRow($"[bold]{stats.Key}[/]", hitCell, missCell, cachedBytesCell, remoteBytesCell);
+                    var valueStats = stats.Value.Stats;
+
+                    var hitCell = $"[lime]{valueStats.Hits.ToMetric()}[/]";
+                    var missCell = $"[red]{valueStats.Misses.ToMetric()}[/]";
+                    var cachedBytesCell = $"[lime]{ByteSize.FromBytes(valueStats.CachedBytes)}[/]";
+                    var remoteBytesCell = $"[red]{ByteSize.FromBytes(valueStats.RemoteBytes)}[/]";
+
+                    if (!itemCountLookup.TryGetValue(stats.Key, out var itemIndex))
+                    {
+                        itemIndex = rowCounter++;
+                        itemCountLookup.Add(stats.Key, itemIndex);
+                        table.AddRow($"[bold]{stats.Key}[/]", hitCell, missCell, cachedBytesCell, remoteBytesCell);
+                    }
+                    else
+                    {
+                        table.UpdateCell(itemIndex, 1, hitCell);
+                        table.UpdateCell(itemIndex, 2, missCell);
+                        table.UpdateCell(itemIndex, 3, cachedBytesCell);
+                        table.UpdateCell(itemIndex, 4, remoteBytesCell);
+                    }
                 }
-                else
-                {
-                    table.UpdateCell(itemIndex, 1, hitCell);
-                    table.UpdateCell(itemIndex, 2, missCell);
-                    table.UpdateCell(itemIndex, 3, cachedBytesCell);
-                    table.UpdateCell(itemIndex, 4, remoteBytesCell);
-                }
+
+                ctx.Refresh();
             }
-
-            ctx.Refresh();
         }
     });
 
